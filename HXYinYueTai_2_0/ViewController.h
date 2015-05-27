d8eef22e80015ef193cb7649a53a6c0b60eb980c
huangxionging <huangxionging@163.com>
2015-05-27 10:03:17 +0800

    创建项目
    创建项目

---
+++ HXYinYueTai_2_0/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HXYinYueTai_2_0
+//
+//  Created by huangxiong on 15/5/26.
+//  Copyright (c) 2015年 New_Life. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HXYinYueTai_2_0Tests/HXYinYueTai_2_0Tests.m
@@ -0,0 +1,40 @@
+//
+//  HXYinYueTai_2_0Tests.m
+//  HXYinYueTai_2_0Tests
+//
+//  Created by huangxiong on 15/5/26.
+//  Copyright (c) 2015年 New_Life. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface HXYinYueTai_2_0Tests : XCTestCase
+
+@end
+
+@implementation HXYinYueTai_2_0Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
